c18dc2bf54059977cff60d82a8402e221b514612
Alberto Rodriguez <ararod9@gmail.com>
2021-01-17 19:53:30 -0500

    updated destination

--- src/main/java/org/example/Destination.java
+++ src/main/java/org/example/Destination.java
@@ -11,4 +11,36 @@ public class Destination {
 	}
 
 	public void allowToVisit(){}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(Double distance) {
+		this.distance = distance;
+	}
+
+	public Double getCostPerMile() {
+		return costPerMile;
+	}
+
+	public void setCostPerMile(Double costPerMile) {
+		this.costPerMile = costPerMile;
+	}
+
+	public Boolean getRequireCovidTest() {
+		return requireCovidTest;
+	}
+
+	public void setRequireCovidTest(Boolean requireCovidTest) {
+		this.requireCovidTest = requireCovidTest;
+	}
 }
--- src/test/java/org/example/DestinationTest.java
+++ src/test/java/org/example/DestinationTest.java
@@ -1,4 +1,44 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class DestinationTest {
-  
+
+	private Destination destination = new Destination();
+
+	@Test
+	void setName() {
+		destination.setName("Madrid, Spain");
+		String expected = "Madrid, Spain";
+		String actual = destination.getName();
+		Assert.assertEquals(expected,actual);
+	}
+
+	@Test
+	void setDistance() {
+		destination.setDistance(3688.00);
+		Double expected = 3688.00;
+		Double actual = destination.getDistance();
+		Assert.assertEquals(expected,actual);
+
+	}
+
+	@Test
+	void setCostPerMile() {
+		destination.setCostPerMile(20.00);
+		Double expected = 20.00;
+		Double actual = destination.getCostPerMile();
+		Assert.assertEquals(expected,actual);
+	}
+
+	@Test
+	void setRequireCovidTest() {
+	}
+
+	@Test
+	void allowToVisit() {
+	}
 }
