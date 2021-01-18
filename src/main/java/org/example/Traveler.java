02cc389d169cf9de647b61fcb4cd4d788695a14d
Alberto Rodriguez <ararod9@gmail.com>
2021-01-17 19:53:55 -0500

    updated travler

--- src/main/java/org/example/Traveler.java
+++ src/main/java/org/example/Traveler.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.exception.InsufficientFundsException;
+
 import java.util.List;
 
 public class Traveler {
@@ -12,5 +14,47 @@ public class Traveler {
 	Traveler(){
 	}
 
-	public void bookATrip(Destination destination){}
+	public void bookATrip(Destination destination)throws InsufficientFundsException {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassPortNo() {
+		return passPortNo;
+	}
+
+	public void setPassPortNo(String passPortNo) {
+		this.passPortNo = passPortNo;
+	}
+
+	public Double getMoney() {
+		return money;
+	}
+
+	public void setMoney(Double money) {
+		this.money = money;
+	}
+
+	public List<Destination> getPlacesVisited() {
+		return placesVisited;
+	}
+
+	public void setPlacesVisited(List<Destination> placesVisited) {
+		this.placesVisited = placesVisited;
+	}
+
+	public CovidResults getCovidResults() {
+		return covidResults;
+	}
+
+	public void setCovidResults(CovidResults covidResults) {
+		this.covidResults = covidResults;
+	}
 }
--- src/test/java/org/example/TravelerTest.java
+++ src/test/java/org/example/TravelerTest.java
@@ -1,4 +1,47 @@
-import static org.junit.jupiter.api.Assertions.*;
-class TravelerTest {
-  
+package org.example;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TravelerTest {
+
+	private Traveler traveler = new Traveler();
+
+	@Test
+	public void setName() {
+		traveler.setName("ryder");
+		String expected = "ryder";
+		String actual = traveler.getName();
+		Assert.assertEquals(expected,actual);
+	}
+
+	@Test
+	public void setPassPortNo() {
+		traveler.setPassPortNo("11");
+		String expected = "11";
+		String actual = traveler.getPassPortNo();
+		Assert.assertEquals(expected,actual);
+	}
+
+	@Test
+	public void setMoney() {
+		traveler.setMoney(25000.00);
+		Double expected = 25000.00;
+		Double actual = traveler.getMoney();
+		Assert.assertEquals(expected,actual);
+	}
+
+	@Test
+	public void setPlacesVisited() {
+
+	}
+
+	@Test
+	public void setCovidResults() {
+
+	}
+
+	@Test
+	public void bookATrip() {
+	}
 }
